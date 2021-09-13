0fa8f04ebc55f13e6eb59c340e9c5f91565d33ca
haraGADygyl <ti60gtt@gmail.com>
2021-09-13 21:22:23 +0300

    Problem Solving

---
+++ 01_Problem_Solving/07_Staircase.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+
+def staircase(n):
+    for i in range(1, n + 1):
+        spaces = n - i
+        print(spaces * ' ' + i * '#')
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
