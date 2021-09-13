dc160c81989b9aa0d1906a50b3ea277034013ebb
haraGADygyl <ti60gtt@gmail.com>
2021-09-13 22:30:01 +0300

    Problem Solving

---
+++ 01_Problem_Solving/09_Birthday_Cake_Candles.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    tallest_candles = 1
+    candles_sorted = list(reversed(sorted(candles)))
+
+    for i in range(candles_count - 1):
+        if candles_sorted[i] == candles_sorted[i + 1]:
+            tallest_candles += 1
+        else:
+            break
+
+    # return tallest_candles
+    print(tallest_candles)
+
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    # fptr.write(str(result) + '\n')
+    #
+    # fptr.close()
