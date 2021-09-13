5cda1b8649682d83e9bb5714d09ccf1ec4393637
haraGADygyl <ti60gtt@gmail.com>
2021-09-13 12:21:55 +0300

    Problem Solving

---
+++ 01_Problem_Solving/05_Diagonal_Difference.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    first_diagonal = sum(arr[i][i] for i in range(n))
+    second_diagonal = sum(arr[i][n - i - 1] for i in range(n))
+
+    return abs(first_diagonal - second_diagonal)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
