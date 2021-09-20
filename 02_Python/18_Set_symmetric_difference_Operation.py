80e9603c15b57e7fdab6868819a70edcaa567dde
haraGADygyl <ti60gtt@gmail.com>
2021-09-20 20:03:15 +0300

    Python

---
+++ 02_Python/18_Set_symmetric_difference_Operation.py
@@ -0,0 +1,8 @@
+students_count_english = int(input())
+student_english_set = set([int(x) for x in input().split()])
+students_count_french = int(input())
+student_french_set = set([int(x) for x in input().split()])
+
+result = student_english_set.symmetric_difference(student_french_set)
+
+print(len(result))
